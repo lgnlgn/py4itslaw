51999585a511d8d22a2199cfbf6fa6ee28d21c62
lgnlgn <gnliang10@126.com>
2018-01-29 22:49:50 +0800

    new commit

--- main.py
+++ main.py
@@ -3,14 +3,15 @@
 import sys
 import os
 from Request import ItslawRequester
-
+import json
 
 data_dir = os.getcwd()
 COURT_MAX = 3568
-year = '2014'
-case_type = '1'
+court_start = 1
 
 crawling_info = {'court_id':0,'total_count': -1, 'finished_idx': 0, 'next_idx':0, 'next_docid': ''}
+
+
 
 def process_argv():
     argvs = len(sys.argv)
@@ -18,7 +19,7 @@ def process_argv():
         sys.stdout("python main.py <year> <caseType> [data_dir]")
         sys.stdout("<caseType> : 1 = min, 2 = xing; default data_dir is './' ")
         return None,None
-    else :
+    else:
         year = sys.argv[1]
         case_type = sys.argv[2]
         if len(year) != 4 or not year.isdigit() or year[0] != '2':
@@ -32,6 +33,15 @@ def process_argv():
             sys.stdout(" set data_dir => " + data_dir)
         return year, case_type
 
+
+def parse_list(list_result):
+    doc = json.loads(list_result)
+    total_count = doc['data']['totalCount']
+    next_id = ''
+    # TODO
+    info = {'total_count': total_count, 'next_docid': next_id, 'next_idx': 1}
+    return info
+
 def main():
     year, case_type = process_argv()
     if year is None or case_type is None:
@@ -39,36 +49,61 @@ def main():
     working_dir = data_dir + os.sep + year + os.sep + case_type
     os.makedirs(working_dir)
 
-    spider = ItslawRequester(caseType, year)
+    spider = ItslawRequester(case_type, year)
 
     court_id = get_last_court(working_dir)
-    next_idx, next_doc = continue_last(working_dir, court_id)
+    next_idx, next_doc = read_info(working_dir, court_id)
+    # new court -> start
+    # new court -> stop
+    # continuous -> start
+    # continuous & last -> stop
+    info = prepare_crawling(court_id, next_idx, next_doc, spider)
 
-    if court_id == 1 and next_idx == 0: #call spider.get_last()
-        pass
 
 
 
 def get_last_court(working_dir):
+    """
+        get max_court_id & create a new info.txt
+    """
+
     courts = os.listdir(working_dir)
     if len(courts) == 0:
-        court = 1
-        os.mkdir(working_dir + "/1") #create first court
-        crawling_info['court_id'] = court
-        f = open(working_dir + "/1/info.txt")
+        os.mkdir(working_dir + os.sep + str(court_start)) #create first court
+        crawling_info['court_id'] = court_start
+        f = open(working_dir + os.sep + str(court_start) + os.sep + "info.txt")
         f.write(str(crawling_info))
         f.close()
-    else:
-        court = max(map(int, courts))
-    return court
 
-def continue_last(working_dir, court_id):
+    # get last court
+    max_court_id = max(map(int,  os.listdir(working_dir)))   # check again without else
+    return max_court_id
+
+
+def read_info(working_dir, court_id):
+    """ reads from info.txt"""
     f = open(working_dir + "/" + str(court_id) + "/info.txt")
     cc = eval(f.read())
     if cc['total_count'] == 0 or cc['total_count'] == cc['finished_idx']: ##end
         return -1,''
     else:
         return cc['next_idx'], cc['next_docid']
+
+
+def prepare_crawling(court_id, next_idx, next_doc, spider):
+    ci = {}
+    ci.update(crawling_info)
+    if court_id == court_start and next_idx == 0: #call spider.get_list()
+        list_result = spider.get_list(court_id)
+        info = parse_list(list_result)
+        info['court_id'] = court_id
+    else:
+        ci
+
+    ci.update(info)
+    return ci
+
+
 
 if __name__ == '__main__':
     working_dir = data_dir + os.sep + year + os.sep + case_type
