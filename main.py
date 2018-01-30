bd647e525351a6549dba62af03fd358fd6d3c84f
lgnlgn <gnliang10@126.com>
2018-01-30 18:17:29 +0800

    aaa

--- main.py
+++ main.py
@@ -34,14 +34,6 @@ def process_argv():
         return year, case_type
 
 
-def parse_list(list_result):
-    doc = json.loads(list_result)
-    total_count = doc['data']['totalCount']
-    next_id = ''
-    # TODO
-    info = {'total_count': total_count, 'next_docid': next_id, 'next_idx': 1}
-    return info
-
 def main():
     year, case_type = process_argv()
     if year is None or case_type is None:
@@ -52,15 +44,30 @@ def main():
     spider = ItslawRequester(case_type, year)
 
     court_id = get_last_court(working_dir)
-    next_idx, next_doc = read_info(working_dir, court_id)
+    while court_id <= COURT_MAX:
+        if court_id == 0 : # only happens at the first time
+            create_info(working_dir, court_id + 1 )
+            court_id += 1
+        info = read_info(working_dir, court_id)
+        if info['total_count'] == -1 :
+            info = prepare_crawling(spider, court_id)
+            continue_crawl(info)
+
+        elif (info['total_count'] == info['finished_idx'] ):
+            pass
+        else:
+            continue_crawl(info)
+        court_id += 1
     # new court -> start
     # new court -> stop
     # continuous -> start
     # continuous & last -> stop
-    info = prepare_crawling(court_id, next_idx, next_doc, spider)
 
 
 
+def continue_crawl(info):
+    # TODO
+    pass
 
 def get_last_court(working_dir):
     """
@@ -70,38 +77,50 @@ def get_last_court(working_dir):
     courts = os.listdir(working_dir)
     if len(courts) == 0:
         os.mkdir(working_dir + os.sep + str(court_start)) #create first court
-        crawling_info['court_id'] = court_start
-        f = open(working_dir + os.sep + str(court_start) + os.sep + "info.txt")
-        f.write(str(crawling_info))
-        f.close()
-
     # get last court
     max_court_id = max(map(int,  os.listdir(working_dir)))   # check again without else
-    return max_court_id
 
+    info = read_info(working_dir, max_court_id)
+
+    return info
+
+def create_info(working_dir, court_id):
+    if not os.path.isfile:
+        ci = {}
+        ci.update(crawling_info)
+        ci['court_id'] = court_id
+        f = open(working_dir + os.sep + str(court_id) + os.sep + "info.txt")
+        f.write(str(ci))
+        f.close()
 
 def read_info(working_dir, court_id):
     """ reads from info.txt"""
-    f = open(working_dir + "/" + str(court_id) + "/info.txt")
+    if not os.path.isfile:
+        return None
+    f = open(working_dir + os.sep + str(court_id) + os.sep + "info.txt")
     cc = eval(f.read())
-    if cc['total_count'] == 0 or cc['total_count'] == cc['finished_idx']: ##end
-        return -1,''
+    f.close()
+    return cc
+##    if cc['total_count'] == 0 or cc['total_count'] == cc['finished_idx']: ##end
+##        return -1,''
+##    else:
+##        return cc['next_idx'], cc['next_docid']
+
+
+def prepare_crawling(spider, court_id):
+
+    list_result = spider.get_list(court_id)
+    doc = json.loads(list_result)
+    total_count = doc['data']['searchResult']['totalCount']
+    if total_count != 0:
+        first = doc['data']['searchResult']['judgements'][0]
+        info = { 'next_docid': first['id'], 'next_idx': 1 }
     else:
-        return cc['next_idx'], cc['next_docid']
-
-
-def prepare_crawling(court_id, next_idx, next_doc, spider):
-    ci = {}
-    ci.update(crawling_info)
-    if court_id == court_start and next_idx == 0: #call spider.get_list()
-        list_result = spider.get_list(court_id)
-        info = parse_list(list_result)
-        info['court_id'] = court_id
-    else:
-        ci
-
-    ci.update(info)
-    return ci
+        info = { 'next_docid': '', 'next_idx': 0 }
+    info['court_id'] = court_id
+    info['finished_idx'] = 0
+    info['total_count'] = total_count
+    return info
 
 
 
